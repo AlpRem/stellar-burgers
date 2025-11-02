57a5647775361627bd824fc38d660e3dc0487575
AlpRem <test@me.comm>
2025-11-02 19:47:05 +0300

    add cypress test from add ingridient and open modal ingridient-details

--- cypress/e2e/ingredients.cy.ts
+++ cypress/e2e/ingredients.cy.ts
@@ -15,11 +15,32 @@ describe('Проверяем функциональность работы с и
 
   it('Проверка вывода ингредиентов с моковыми данными', function () {
     cy.wait('@getListIngredients').its('response.statusCode').should('eq', 200);
-    cy.get('[data-cy=ingredient-container]').should('have.length.at.least', 1);
+    cy.get('[data-cy=cy-ingredient]').should('have.length.at.least', 1);
     cy.fixture('ingredients.json').then((data) => {
-      data.data.forEach((ingredient: { name: string | number | RegExp }) => {
-        cy.get('[data-cy=ingredient-container]').contains(ingredient.name);
+      data.data.forEach((ingredient: { name: string }) => {
+        cy.get('[data-cy=cy-ingredient]').should(
+          'contain.text',
+          ingredient.name
+        );
       });
     });
+  });
+
+  it('Проверка добавления ингредиента из списка в конструктор', function () {
+    cy.get('[data-cy=cy-ingredient]').first().as('ingredient');
+    cy.get('@ingredient')
+      .find('[data-cy=cy-ingredient-name]')
+      .invoke('text')
+      .then((ingredientName) => {
+        cy.get('@ingredient').find('button').click();
+        cy.get('[data-cy=cy-bun]')
+          .should('exist')
+          .and('contain.text', ingredientName);
+      });
+  });
+
+  it('Проверка открытие модального окна ингредиента', function () {
+    cy.get('[data-cy=cy-ingredient]').first().click();
+    cy.get('[data-cy=cy-ingredient-details]').should('exist');
   });
 });
